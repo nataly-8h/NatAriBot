5ad87d5f9d162b2d89f372f68eedd8e7fbc9b82f
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2019-11-04 17:37:32 -0600

    h8
    h8 furro

---
+++ src/wof.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import javax.swing.*;
+public class wof extends JFrame{
+	public wof() {
+		super("WOOOF OVERDRIVE");
+		this.setSize(720, 700);
+		
+		JTextField e = new JTextField();
+		add(e);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawOval(100,200, 100, 100);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WOF");
+		wof a = new wof();
+	
+	}
+
+}
